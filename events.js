02da364c9b5c10cce02fd79636e1adf0c00af925
Markus Loukiainen <markusloukiainen1@gmail.com>
2020-01-29 14:14:14 +0200

    click test

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent('markerhandler', {
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.getIntersection;
+            const intersectedElement = ev && ev.detail && ev.detail.getIntersection(el);
             if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
