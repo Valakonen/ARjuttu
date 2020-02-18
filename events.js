133c79e9820a4e0d57658c8986de570281a33cb9
Markus Loukiainen <markusloukiainen1@gmail.com>
2020-02-18 12:08:00 +0200

    + json for products shows now products info when detected with marker.

--- events.js
+++ events.js
@@ -8,6 +8,20 @@ $(document).on("click",".ingredientTitle",function(){
     $(this).next().slideToggle(200);
 });
 
+// Get products json file
+let products;
+
+fetch("./products.json")
+  .then(function(resp)
+{
+  return resp.json();
+})
+.then(function(data)
+{
+  console.log(data);
+  products = data;
+})
+
 // Info screen toggle
 function toggleInfo()
 {
@@ -22,6 +36,16 @@ function toggleInfo()
     document.getElementById("info").style.display = "block";
   }
   audioInfoBtn.play();
+};
+
+// change product info by marker number
+function changeProductInfo(markerNumber)
+{
+  removeIngredients();
+  document.getElementById("productName").innerHTML = products[markerNumber].name;
+  for (var x of products[markerNumber].ingredients) {
+    addIngredients(x.name, x.info);
+  }
 };
 
 // remove all ingredients
@@ -42,24 +66,11 @@ function removeIngredients()
 };
 
 // add ingredients
-function addIngredients(productNumber)
+function addIngredients(name, info)
 {
-  var a;
-  var b;
-  if (productNumber == 1)
-  {
-    a = "SITRUUNARUOHO";
-    b = "Sitruunaruoho tai sitruunaheinä on arvostettu ja monipuolinen kasvi, joka kasvaa tropiikissa kahden metrin korkuiseksi. Sitruunaruoho kuuluu cymbog on - sukuun, jossa on yli 50 eri lajoa . Suomessa ei juurikaan tehdä eroja eri lajien välillä ja yleinen nimi on sitruunaruoho. C ymbogon citratus on se yleinen syötävä sitruunaruoho , kun taas Cymbogon nardus (jäkkisitrusheinä tai sitronellaheinä) on suosittua kosmetiikassa ja jopa hyönteisten k arkottajana. Cymbogon narduksella on purppuranväriset varre t. Siitä saadaan eteeristä öljyö , jota käytetään saippuoissa, kynttilöissä ja aromaterapiassa. Tärkeimmät kemialliset aineet ovat sitraali eli lemonaali ja geranioli, jotka ovat antiseptisiä. Sit raali antaa sitrusmaisen tuoksun ja sama aine tta on myös sitruunamelissassa. Sitraalia ja geraniolia käytetään paljon parfyymeissa. Eteerinen öljy saadaan tuoreista varsis ta ja lehdistä höyrytislaamalla.";
-  }
-  else if (productNumber == 2)
-  {
-    a = "MUSTAHERUKKA";
-    b = "Mustaherukkaan (ribes nigrum) kuuluu 150 eri lajia, joita tavataan Euroopassa ja Pohjois - Amerikassa. Se on kotoisin pohjoisesta Euroopasta ja Aasiasta. Sitä viljeltiin jo 1000 - luvulla Venäjän luostareissa. Mustaherukassa olevat fenoliset yhd isteet pystyvät estämään useiden eri bakteerien sitoutumisen elimistö ön. Mustikan, mustaherukan , variksenmarjan ja puolukan polyfenolit (antosyaanit, proantosyanidit ja flavonolit) torjuvat suun bakteereja ja estävät karieksen ja plakin muodostumista hampa isiin. Mustaherukan siemenistä puristettu öljy sopii kuiville hiuksille ja iholle. Se sisältää gammalinoleenihappoa, joka on omega - 6 rasvahappo.";
-  }
-
   var title = document.createElement("h2");
   title.classList.add('ingredientTitle');
-  var titleText = document.createTextNode(a);
+  var titleText = document.createTextNode(name);
   title.appendChild(titleText);
 
   var element = document.getElementById("ingredients");
@@ -67,15 +78,10 @@ function addIngredients(productNumber)
 
   var text = document.createElement("p");
   text.classList.add('ingredientText');
-  var ingredientText = document.createTextNode(b);
+  var ingredientText = document.createTextNode(info);
   text.appendChild(ingredientText);
   element.appendChild(text);
 };
-
-function changeProductInfo(markerNumber)
-{
-
-}
 
 window.addEventListener('camera-init', (data) => {
     console.log('camera-init', data);
@@ -98,21 +104,13 @@ AFRAME.registerComponent('registerevents', {
         {
           var audio = new Audio('Sound_2.mp3');
           audio.play();
-
-          removeIngredients();
-          addIngredients(1);
-          document.getElementById("productName").innerHTML = "Tuote 1";
-          //sound2.components.sound.playSound();
+          changeProductInfo(markerValue);
         }
         else if (markerValue == 1)
         {
           var audio2 = new Audio('Sound_2.mp3');
           audio2.play();
-
-          removeIngredients();
-          addIngredients(2);
-          document.getElementById("productName").innerHTML = "Tuote 2";
-          //sound.components.sound.playSound();
+          changeProductInfo(markerValue);
         }
 
 			});
